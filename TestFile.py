7db0e72863f52f78f275fd665e46474c15f18625
Jonathan Markman <jmarkman446@gmail.com>
2020-07-25 14:06:04 -0400

    fix syntax error and refactor application code

--- TestFile.py
+++ TestFile.py
@@ -1,5 +1,17 @@
 ### Elite Hacker Code####
 import constants
+import csv
 
-pokimane="Yeah I fucked her."
-print(GUY_WHO_FUCKS_MEN + "is gay".)
+def getEGirls():
+    with open('egirls.csv', mode='r') as egirlsCsv:
+        csvReader = csv.reader(egirlsCsv)
+        egirls = [{"name": row[0], "fucked": int(row[1])} for row in csvReader]
+    return egirls
+
+egirlList = getEGirls()
+
+for girl in egirlList:
+    if girl["fucked"] == 1:
+        print(f"{girl['name']}? Yeah I fucked her.")
+
+print(f"{constants.GUY_WHO_FUCKS_MEN} is gay.")
